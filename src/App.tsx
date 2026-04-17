b04be1697fd2f34794f8781ab5e46c1d824a0a43
JYOTHISH T M <jothishtm2002@gmail.com>
2026-04-17 20:57:14 +0530

    feat: integrate home page and layout components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/HomePage";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/home" element={<HomePage />} />
         </Routes>
       </div>
     </BrowserRouter>
