9b2324049f2a4267e36c19eac984ae91adfbbee9
JYOTHISH T M <jothishtm2002@gmail.com>
2026-04-21 22:27:20 +0530

    chore: update app entry configuration

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import App from './App.tsx'
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById('root')!).render(
-<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-  <StrictMode>
-    <App />
-  </StrictMode>
-</GoogleOAuthProvider>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GoogleOAuthProvider>
 )
